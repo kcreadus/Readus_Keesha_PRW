babda190b1edce4edd9cfd37f01a2abe488efb7e
KeeshaR <kcreadus@yahoo.com>
2022-02-07 01:44:41 -0600

    components start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+//ADD IMAGES
+import AvatarIcon from '../src/images/1female.png';
+import MyAvatar from '../src/components/MyAvatar';
+
+//REACT icons
+import { GiWomanElfFace } from "react-icons/gi";
+
 
 class App extends Component() {
   render() {
     return (
-      <div className="App">
-        <p></p>
+      <div style={styles.container}>
+        <div style={styles.box}>
+          <h1>Avatar Examples:</h1>
+          <MyAvatar AvatarIcon={AvatarIcon}/>
+          <img src={AvatarIcon} alt="Logo" style={styles.logo} />
+        </div>
+          <div style={styles.box}>
+           <h1>Icon Examples:</h1>
+           <p style={styles.icons}>
+             <GiWomanElfFace />
+           </p>
+          </div>
+        
       </div>
     );
   }
 }
 export default App;
+
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  box:{
+    backgroundColor: 'lightgrey',
+    borderRadius: '2%',
+    padding: '2%',
+    marginRight: '2%'
+  },
+  avatar: {
+    padding: '2%'
+  },
+  logo: {
+    width: '50px',
+    height: '50px'
+  },
+  icons:{
+    fontSize: '2rem',
+  }
+
+}
---
+++ src/components/MyAvatar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+//Dummy Component
+const MyAvatar = props =>{
+    return(
+        <img src={props.AvatarIcon} alt={props.AvatarAlt} style={styles.myAvatar} />
+    )
+}
+export default MyAvatar
+
+const styles={
+    myAvatar: {
+        height: '75px',
+        cursor: 'pointer',
+        borderRadius: '50%'
+    }
+
+}
