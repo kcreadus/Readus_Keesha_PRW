653d4a610b47f05dcce66f9cf3d6ccbc49a35c33
KeeshaR <kcreadus@yahoo.com>
2022-02-09 12:38:36 -0600

    Changed Layout

--- src/App.js
+++ src/App.js
@@ -1,55 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //ADD IMAGES
-import AvatarIcon from '../src/images/1female.png';
-import MyAvatar from '../src/components/MyAvatar';
+import AvatarIcon from "../src/images/avatar.png";
+import MyAvatar from "./components/MyAvatar";
 
 //REACT icons
 import { GiWomanElfFace } from "react-icons/gi";
 
-
 class App extends Component() {
   render() {
     return (
-      <div style={styles.container}>
-        <div style={styles.box}>
-          <h1>Avatar Examples:</h1>
-          <MyAvatar AvatarIcon={AvatarIcon}/>
-          <img src={AvatarIcon} alt="Logo" style={styles.logo} />
-        </div>
-          <div style={styles.box}>
-           <h1>Icon Examples:</h1>
-           <p style={styles.icons}>
-             <GiWomanElfFace />
-           </p>
-          </div>
-        
-      </div>
-    );
+      
+    )
   }
 }
 export default App;
 
-
 const styles = {
   container: {
-    display: 'flex',
-    flexDirection: 'row'
+    display: "flex",
+    flexDirection: "row",
   },
-  box:{
-    backgroundColor: 'lightgrey',
-    borderRadius: '2%',
-    padding: '2%',
-    marginRight: '2%'
+  box: {
+    backgroundColor: "lightgrey",
+    borderRadius: "2%",
+    padding: "2%",
+    marginRight: "2%",
   },
   avatar: {
-    padding: '2%'
+    padding: "2%",
   },
   logo: {
-    width: '50px',
-    height: '50px'
+    width: "50px",
+    height: "50px",
   },
-  icons:{
-    fontSize: '2rem',
-  }
-
-}
+  icons: {
+    fontSize: "2rem",
+  },
+};
---
+++ src/components/header/MyHeader.js
@@ -0,0 +1,83 @@
+import React from 'react';
+// import Component
+import ImageUrl from '../images/avatar.png';
+import  MyIcons from './MyIcons';
+
+
+const  MyHeader = props => {
+    return(
+        <header style={styles.myHeader}>
+          <div style={styles.left}>
+              <div style={styles.logo}></div>
+                  <h1>Natural Hair Care</h1>
+              </div>
+              <div style={styles.searchCont}>
+                  <span style={styles.inputIcon}><Icon /></span>
+                  <input
+                  type='text'
+                  style={styles.inputwithIcon}
+                  placeholder='Search' />
+              </div>
+              <div style={styles.rightside}>
+                  <button style={styles.rightIcons}><Icon /></button>
+                  <button style={styles.rightIcons}><Icon /></button>
+              </div>
+        </header>
+    )
+}
+
+export default MyHeader
+
+const styles={
+    myHeader:{
+        // margin: '1';
+        padding: '1%',
+        backgroundColor: 'black',
+        color: 'rgb(163,173,194)',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        boxShadow: '1px 6px 1px 1px rgba(0, 0, 255, .1)'
+
+    },
+    left: {
+        display: 'flex',
+        flexDirection: 'row',
+        //width: '100%',
+        //marginBottom: '10px'
+    },
+    logo: {
+        borderRadius: '50%',
+        height: '50px',
+        width: '60px',
+        marginRight: '10%',
+        backgroundColor: 'white',
+        alignItems: 'center',
+    },
+    searchCont: {
+        border: '1px solid #ddd',
+        display: 'flex',
+        flexDirection: 'row',
+        borderRadius: '2px',
+        width: '40%'
+    },
+    inputIcon:{
+        background:'#ddd',
+        padding: '10px',
+        display: 'flex',
+        alignItems: 'center',
+        fontSize: '1rem'
+    },
+    inputwithIcon:{
+        border: 'none',
+        flex: 1,
+        padding: '10px'
+    },
+    rightSide:{
+        display: 'flex',
+        flexDirection: 'row',
+    }
+}
+
+
