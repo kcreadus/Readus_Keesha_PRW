983bef7041d673c45832fb8017b0e4066367c0a1
KeeshaR <kcreadus@yahoo.com>
2022-02-27 12:10:51 -0600

    styling added

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -8,27 +8,63 @@ const UserForm = (props) => {
         <input type="text" value={props.userName} placeholder="User Name:" />
       </div>
       <form>
-        <div style={styles.input2}>
-          <input type="text" value={props.fName} placeholder="First Name:" />
-          <input type="text" value={props.lName} placeholder="Last Name:" />
-        </div>
-        <div style={styles.input1}>
-          <input type="text" value={props.address} placeholder="Address:" />
-        </div>
-        <div style={styles.input2}>
-          <input type="text" value={props.city} placeholder="City:" />
-          <input type="text" value={props.state} placeholder="State:" />
-        </div>
-        <div style={styles.input2}>
-          <input type="text" value={props.zipCode} placeholder="Postal Code:" />
+        <div style={styles.inputContainer}>
           <input
             type="text"
+            style={styles.input2}
+            value={props.fName}
+            placeholder="First Name:"
+          />
+          <input
+            type="text"
+            style={styles.input2}
+            value={props.lName}
+            placeholder="Last Name:"
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            style={styles.input1}
+            value={props.address}
+            placeholder="Address:"
+          />
+        </div>
+        <div style={styles.inputContainer}>
+          <input
+            type="text"
+            style={styles.input2}
+            value={props.city}
+            placeholder="City:"
+          />
+          <input
+            type="text"
+            style={styles.input2}
+            value={props.state}
+            placeholder="State:"
+          />
+        </div>
+        <div style={styles.inputContainer}>
+          <input
+            type="text"
+            style={styles.input2}
+            value={props.zipCode}
+            placeholder="Postal Code:"
+          />
+          <input
+            type="text"
+            style={styles.input2}
             value={props.pNumber}
             placeholder="Phone Number:"
           />
         </div>
-        <div style={styles.input1}>
-          <input type="text" value={props.password} placeholder="Password:" />
+        <div>
+          <input
+            type="text"
+            style={styles.input1}
+            value={props.password}
+            placeholder="Password:"
+          />
         </div>
       </form>
     </div>
@@ -43,13 +79,43 @@ const styles = {
     flexDirection: "column",
     width: "30%",
     height: "60%",
-    backgroundColor: "whiteSmoke",
+    backgroundColor: "#7785AC",
     color: "black",
   },
   input1: {
     display: "flex",
+    width: "91%",
+    padding: "10px",
+    fontWeight: "normal",
+    background: "none",
+    border: "1px solid #8FA4B9",
+    color: "#fff",
+    outline: "none",
+    fontSize: "14px",
+    marginTop: "2%",
+    marginRight: "2%",
+    marginBottom: "2%",
+    marginLeft: "2%",
+    transition: "0.5s all",
   },
   input2: {
     display: "flex",
+    width: "42%",
+    padding: "10px",
+    fontWeight: "normal",
+    background: "none",
+    border: "1px solid #8FA4B9",
+    color: "#fff",
+    outline: "none",
+    fontSize: "14px",
+    marginTop: "2%",
+    marginRight: "2%",
+    marginBottom: "2%",
+    marginLeft: "2%",
+    transition: "0.5s all",
+  },
+  inputContainer: {
+    display: "flex",
+    justifyContent: "space-between",
   },
 };
